6656a7df68b23a3963ae209e0a70be000cdf421f
vqtung2k4 <vuquangtung2004@gmail.com>
2022-07-11 16:38:32 +0700

    Căn chỉnh một số Element

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const timeLeft = document.getElementById('time-left')
+const timeLeftHp = document.getElementById('time-left-Hp')
+const timeLeftHp1 = document.getElementById('time-left-Hp1')
 
-const birthday = new Date('07/29/2022')
+const birthday = new Date('07/11/2022')
 const second = 1000
 const minute = second * 60
 const hour = minute * 60
@@ -14,12 +16,12 @@ function countDown() {
 	const timeSpan = birthday - today
 
 	if(timeSpan<= -day) {
-		timeLeft.innerHTML = "Hope you had a nice Birthday"
+		timeLeftHp.innerHTML = "Hi vọng cậu đã có một ngày sinh nhật đáng nhớ!"
 		clearInterval(timerId)
 		return
 	}
 	if (timeSpan <= 0) {
-		timeLeft.innerHTML = "Happy Birthday!!!"
+		timeLeftHp1.innerHTML = "Chúc mừng sinh nhật cậu, Nguyễn Khánh Vy!"
 		clearInterval(timerId)
 		return
 	}
